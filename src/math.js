03f22582052f8c3dc22af82105c0dc20f3229b03
Deepak Jois <deepak.jois@gmail.com>
2016-06-14 15:15:30 +0530

    ESLint related fixes

--- src/math.js
+++ src/math.js
@@ -1,8 +1,8 @@
-export const sqrt = Math.sqrt;
+export const sqrt = Math.sqrt
 
-export function square(x) {
-    return x * x;
+export function square (x) {
+  return x * x
 }
-export function diag(x, y) {
-    return sqrt(square(x) + square(y));
+export function diag (x, y) {
+  return sqrt(square(x) + square(y))
 }
