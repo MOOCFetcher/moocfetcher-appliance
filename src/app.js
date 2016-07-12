c3905270853a8e58bde7d72992a04e88cb22f89e
Deepak Jois <deepak.jois@gmail.com>
2016-07-12 13:24:28 +0530

    Indentation

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <div>
-  <p className="text-xs-center">Search or browse our catalog of courses and select them to copy onto your own USB pen&nbsp;drive.</p>
+    <p className="text-xs-center">Search or browse our catalog of courses and select them to copy onto your own USB pen&nbsp;drive.</p>
   </div>,
   document.getElementById('app')
 )
