fdb0c3520743c924ee7b9a540f04e25e64727a1f
Deepak Jois <deepak.jois@gmail.com>
2016-07-21 22:23:15 +0530

    Add functionality to SearchBox

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import {CourseActions} from './CourseStore'
 
 export default class SearchBox extends React.Component {
+
+  initiateFilter = (e) => {
+    CourseActions.filter(e.target.value)
+  }
+
   render() {
     return (
       <form>
-        <input type="text" className="form-control" id="search-box" placeholder="e.g. Philosophy"/>
+        <input type="text" className="form-control" id="search-box" ref="searchbox" placeholder="e.g. Philosophy" onInput={this.initiateFilter}/>
       </form>
     )
   }
--- src/__tests__/SearchBox-test.js
+++ src/__tests__/SearchBox-test.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import TestUtils from 'react-addons-test-utils'
+import SearchBox from '../SearchBox'
+import {CourseActions} from '../CourseStore'
 
 jest.unmock('../SearchBox')
 
 
 describe('SearchBox', () => {
   let item
+  let term = "Philosophy"
   beforeEach(() => {
     item = TestUtils.renderIntoDocument(<SearchBox/>)
   })
 
   it('Filters course when oninput event is fired for the search input field', () => {
-    item.refs.searchbox.value = "Philosophy"
+    item.refs.searchbox.value = term
+    TestUtils.Simulate.input(item.refs.searchbox)
+    expect(CourseActions.filter.mock.calls.length).toBe(1)
+    expect(CourseActions.filter.mock.calls[0][0]).toBe(term)
   })
 })
