51ac03cee14bbe48eb8537eadf8b71cd219448e4
Deepak Jois <deepak.jois@gmail.com>
2016-08-03 18:35:08 +0530

    Fix CORS Handler after actual testing

--- backend/moocfetcher-server/main.go
+++ backend/moocfetcher-server/main.go
@@ -126,7 +126,12 @@ func statsHandler(w http.ResponseWriter, r *http.Request) {
 
 func addCorsHeaders(h http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		if r.Method == "OPTIONS" {
 			return
 		}
